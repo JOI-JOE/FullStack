57428aed52280d2e1cbb48d1e8c5f4d3f23aa997
JOI-JOE <iknowhtml161gmail.com>
2024-07-03 10:32:34 +0700

    migration ...

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -6,9 +6,14 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("Users", [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "example@example.com",
+        email: "haungodan@example.com",
+        password: "123456",
+        firstName: "Vito",
+        lastName: "Coleone",
+        address: "Ha Noi",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
